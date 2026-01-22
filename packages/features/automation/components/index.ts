3dd8179c8bc3083b2c8e5bd9fa2e1a3e26722c72
Takahiro Motoyama <t.ndc16@gmail.com>
2026-01-22 09:46:15 +0900

    feat(automation): add automation dashboard with viral post management (#117)

---
+++ packages/features/automation/components/index.ts
@@ -0,0 +1,6 @@
+export * from './ViralPostQueue';
+export * from './RepostSchedule';
+export * from './AutoDeleteSettings';
+export * from './AutoPlugSettings';
+export * from './RepostHistory';
+export * from './AutomationDashboard';
